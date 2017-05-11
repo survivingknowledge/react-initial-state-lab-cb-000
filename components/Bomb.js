7b866156df658039aaf4a3c6204b040263594fa8
Jordan Sleeper <ezirens@gmail.com>
2017-05-11 22:34:50 +0000

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Bomb extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  render(){
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>
+        {message}
+      </div>
+    );
+  }
+
+}
---
+++ components/ImageSlider.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class ImageSlider extends React.Component{
+  constructor(props){
+    super();
+
+    this.state = {
+      currentSlideIndex: 0,
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        I am on slide {this.state.currentSlideIndex}
+      </div>
+    );
+  }
+
+}
